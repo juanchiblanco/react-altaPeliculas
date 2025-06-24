bd665f3109b2a7e07a6b05aac7cba5a05c75a635
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-24 19:14:58 -0300

    disenio de lista

---
+++ src/components/ListaPeliculas.jsx
@@ -0,0 +1,15 @@
+
+const ListaPeliculas = () => {
+    return (
+        <article className="w-75 border border-light-subtle">
+            <div className="row justify-content-center">
+                <p className="col-12 col-md-6 col-lg-3">1</p>
+                <p className="col-12 col-md-6 col-lg-3">1</p>
+                <p className="col-12 col-md-6 col-lg-3">1</p>
+                <p className="col-12 col-md-6 col-lg-3">1</p>
+            </div>
+        </article>
+    );
+};
+
+export default ListaPeliculas;
