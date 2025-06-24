36d70c84454449120ba426d3d6f16e8aa80d604c
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-24 18:51:09 -0300

    disenio del form de peliculas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import FormularioPelicula from "./components/FormularioPelicula";
 
 function App() {
   return (
     <main className="container my-3">
-      <h1 className="text-center">Hola Mundo</h1>
+      <h1 className="text-center">Cartelera</h1>
+       <section className="row justify-content-center">
+        <FormularioPelicula/>
+       </section>
     </main>
   )
 }
---
+++ src/components/FormularioPelicula.jsx
@@ -0,0 +1,46 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+const FormularioPelicula = () => {
+  return (
+    <div className="w-75 border border-light-subtle rounded p-3 my-3">
+      <Form>
+        <Form.Group className="mb-3" controlId="nombrePelicula">
+          <Form.Label>Nombre de la pelicula *</Form.Label>
+          <Form.Control type="text" placeholder="Ej: Interestellar" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="descripcion">
+          <Form.Label>Descripcion *</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={5}
+            placeholder="Ej: Gracias a un descubrimiento, un grupo de científicos y exploradores, encabezados por Cooper, se embarcan en un viaje espacial para encontrar un lugar con las condiciones necesarias para reemplazar a la Tierra y comenzar una nueva vida allí."
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="nombreMascota">
+          <Form.Label>Genero *</Form.Label>
+          <Form.Select aria-label="Default select example">
+            <option>Elige una opcion</option>
+            <option value="1">Acción</option>
+            <option value="2">Aventura</option>
+            <option value="3">Ciencia ficción</option>
+            <option value="4">Comedia</option>
+            <option value="5">Drama</option>
+            <option value="6">Terror</option>
+            <option value="7">Fantasía</option>
+            <option value="8">Musical</option>
+            <option value="9">Romántico</option>
+            <option value="10">Thriller</option>
+            <option value="11">Animación </option>
+            <option value="12">Documental</option>
+          </Form.Select>
+        </Form.Group>
+        <Button variant="primary" type="submit" className="mt-4 w-100 shadow">
+          Agregar nueva cita
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default FormularioPelicula;
